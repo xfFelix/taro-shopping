d4b5e5edb26b468f8ebbca86e9e717172255f240
front <front123>
2019-04-08 12:00:45 +0800

    首页

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <input name="" id="" placeholder="测试number"  v-enter-number type="number" pattern="[0-9]*" autocomplete="off">
+      
   </div>
 </template>
 <script>
