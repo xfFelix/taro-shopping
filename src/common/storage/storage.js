4e96312b3157e2a49a7a732c40525c5a30938443
xfFlix <13590344852@163.com>
2019-06-18 13:04:11 +0800

    修改food.html

--- src/common/storage/storage.js
+++ src/common/storage/storage.js
@@ -21,42 +21,82 @@ class Storage {
     this.prefix = storage_prefix
   }
 
-  set (key, value) {
-    try {
-      value = JSON.stringify(value)
-    } catch (e) {
-      value = value
+  // set (key, value) {
+  //   try {
+  //     value = JSON.stringify(value)
+  //   } catch (e) {
+  //     value = value
+  //   }
+
+  //   this.store.setItem(this.prefix + key, value)
+
+  //   return this
+  // }
+
+  // get (key) {
+  //   if (!key) {
+  //     throw new Error('没有找到key。')
+  //   }
+  //   if (typeof key === 'object') {
+  //     throw new Error('key不能是一个对象。')
+  //   }
+  //   let value = this.store.getItem(this.prefix + key)
+
+  //   if (value === null) {
+  //     return undefined
+  //   }
+
+  //   try {
+  //     value = JSON.parse(value)
+  //   } catch (e) {
+  //     value = value
+  //   }
+  //   return value
+  // }
+
+  // remove (key) {
+  //   this.store.removeItem(this.prefix + key)
+  //   return this
+  // }
+
+  /**
+   对本地数据进行操作的相关方法，如localStorage,sessionStorage的封装
+  */
+  set(key, value, duration) {
+    if (key === 'token') {
+      return this.store[this.prefix + key] = JSON.stringify(value);
     }
-
-    this.store.setItem(this.prefix + key, value)
-
-    return this
+    var data = {
+      value: value,
+      expiryTime: !duration || isNaN(duration) ? 0 : this.getCurrentTimeStamp() + parseInt(duration)
+    };
+    this.store[this.prefix + key] = JSON.stringify(data);
   }
-
-  get (key) {
-    if (!key) {
-      throw new Error('没有找到key。')
+  get(key) {
+    if (key === 'token') {
+      return JSON.parse(this.store[this.prefix + key])
     }
-    if (typeof key === 'object') {
-      throw new Error('key不能是一个对象。')
+    var data = this.store[this.prefix + key];
+    if (!data || data === "null") {
+      return null;
     }
-    let value = this.store.getItem(this.prefix + key)
-
-    if (value === null) {
-      return undefined
-    }
-
+    var now = this.getCurrentTimeStamp();
+    var obj;
     try {
-      value = JSON.parse(value)
+      obj = JSON.parse(data);
     } catch (e) {
-      value = value
+      return null;
     }
-    return value
+    if (obj.expiryTime === 0 || obj.expiryTime > now) {
+      return obj.value;
+    }
+    return null;
   }
-
-  remove (key) {
-    this.store.removeItem(this.prefix + key)
-    return this
+  remove(key){
+    this.store.removeItem(this.prefix + key);
+  }
+  getCurrentTimeStamp() {
+    return Date.parse(new Date());
   }
 }
 
