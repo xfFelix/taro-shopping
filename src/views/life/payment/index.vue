e8834eec9830052adc942459d04bff2eeb8ce505
xfFlix <13590344852@163.com>
2019-08-08 17:46:00 +0800

    修改充值条件

--- src/views/life/payment/index.vue
+++ src/views/life/payment/index.vue
@@ -166,6 +166,9 @@ export default {
       if (+this.price < 5 || +this.price > 9999) {
         return this.$toast('金额限制5~9999')
       }
+      if (this.arrears && this.arrears > this.price) {
+        return this.$toast('充值金额要大于欠费金额')
+      }
       let res = await this.checkPassword()
       if (!res) return
       this.showCode = true
